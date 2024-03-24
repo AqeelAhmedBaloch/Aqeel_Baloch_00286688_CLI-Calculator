cbbd2bf1abcde468cadbc0d7fbd6ab0e39bbf32d
Aqeel Ahmed Baloch <151179923+AqeelAhmedBaloch@users.noreply.github.com>
2024-03-24 14:51:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// Print Title Aqeel Ahmed Baloch
+console.log('\n\tAqeel Ahmed Baloch CLI-Calculator\n');
+const answer = await inquirer.prompt([
+    { message: "Enter First Number  :", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number :", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// conditional if statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select valid operator");
+}
+;
